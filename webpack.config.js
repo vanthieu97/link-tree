791e40db9693350b635fadf0fcb52f5d3d0930c6
vanthieu97 <vanthieunguyen234@gmail.com>
2022-09-15 01:49:31 +0700

    fist commit

---
+++ webpack.config.js
@@ -0,0 +1,44 @@
+const path = require("path");
+const CopyWebpackPlugin = require("copy-webpack-plugin");
+const HtmlWebpackPlugin = require("html-webpack-plugin");
+const MiniCssExtractPlugin = require("mini-css-extract-plugin");
+const CssMinimizerPlugin = require("css-minimizer-webpack-plugin");
+const HTMLInlineCSSWebpackPlugin =
+  require("html-inline-css-webpack-plugin").default;
+
+module.exports = {
+  devServer: {
+    port: 3000,
+    watchFiles: ["script.js"],
+    historyApiFallback: true,
+  },
+  mode: process.env.MODE,
+  entry: "./script.js",
+  output: {
+    filename: "script.js",
+    path: path.resolve(__dirname, "build"),
+    publicPath: "/",
+  },
+  module: {
+    rules: [
+      {
+        test: /\.css$/,
+        use: [MiniCssExtractPlugin.loader, "css-loader"],
+      },
+    ],
+  },
+  optimization: {
+    minimizer: [new CssMinimizerPlugin()],
+  },
+  plugins: [
+    new CopyWebpackPlugin({
+      patterns: [{ from: "images", to: "images" }],
+    }),
+    new MiniCssExtractPlugin(),
+    new HtmlWebpackPlugin({
+      favicon: "./images/favicon.png",
+      template: "index.html",
+    }),
+    new HTMLInlineCSSWebpackPlugin(),
+  ],
+};
