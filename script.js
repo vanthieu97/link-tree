9da7722a34e161e1faa6fddf64a2a13ad1b27f1b
vanthieu97 <vanthieunguyen234@gmail.com>
2022-09-19 00:02:51 +0700

    Updates

--- script.js
+++ script.js
@@ -1,44 +1,6 @@
-/******/ (() => { // webpackBootstrap
-/******/ 	"use strict";
-var __webpack_exports__ = {};
-
-window.onload = async function () {
-  const container = document.querySelector(".container");
-  const navProducts = document.getElementById("navProducts");
-  const navContacts = document.getElementById("navContacts");
-  const productsElement = document.getElementById("products");
-  const contactsElement = document.getElementById("contacts");
-  navProducts.addEventListener("click", () => {
-    container.scrollTo({
-      behavior: "smooth",
-      top: productsElement.offsetTop,
-    });
-  });
-  navContacts.addEventListener("click", () => {
-    container.scrollTo({
-      behavior: "smooth",
-      top: contactsElement.offsetTop,
-    });
-  });
-  try {
-    const res = await fetch("https://api.npoint.io/9e5188f459ca27a8100c");
-    const data = await res.json();
-    const { products } = data;
-    productsElement.querySelector(".items").innerHTML = [...products].reduce(
-      (cur, item) =>
-        cur +
-        `<a href="${item.url}">
+(()=>{"use strict";window.onload=async function(){console.log("jenkins: 001");const t=document.querySelector(".container"),e=document.getElementById("navProducts"),o=document.getElementById("navContacts"),c=document.getElementById("products"),n=document.getElementById("contacts");e.addEventListener("click",()=>{t.scrollTo({behavior:"smooth",top:c.offsetTop})}),o.addEventListener("click",()=>{t.scrollTo({behavior:"smooth",top:n.offsetTop})});try{const s=await fetch("https://api.npoint.io/9e5188f459ca27a8100c");var i=(await s.json())["products"];c.querySelector(".items").innerHTML=[...i].reduce((t,e)=>t+`<a href="${e.url}">
         <div class="item">
-            <img src="${item.image}" alt="${item.title}" />
-            <div class="item-title">${item.title}</div>
+            <img src="${e.image}" alt="${e.title}" />
+            <div class="item-title">${e.title}</div>
         </div>
-    </a>`,
-      ""
-    );
-    document.querySelector(".loading").style.display = "none";
-    productsElement.querySelector(".items").style.display = "block";
-  } catch (err) {}
-};
-
-/******/ })()
-;
+    </a>`,""),document.querySelector(".loading").style.display="none",c.querySelector(".items").style.display="block"}catch(t){}}})();
